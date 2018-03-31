840c19d1a8843c3d3177ec87ccad86ebb273f125
Ernestas Poskus <ernestas.poskus@gmail.com>
2018-03-31 10:13:58 +0300

    Add security groups

---
+++ security.tf
@@ -0,0 +1,40 @@
+resource "aws_security_group" "instances" {
+  name   = "sec_instance"
+  vpc_id = "${aws_vpc.site.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "sec_elb"
+
+  vpc_id = "${aws_vpc.site.id}"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  depends_on = ["aws_internet_gateway.gateway"]
+}
